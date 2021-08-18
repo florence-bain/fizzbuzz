169f47bb0717fc896cb9389a789de11f2d4d5095
Jordan <jordanveness8@gmail.com>
2021-08-18 15:43:00 +0100

    "functioning fizzbuzz test"

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,19 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizzbuzz" when passed 3 and 5' do
+    fizz_15 = fizzbuzz(15)
+
+    expect(fizz_15).to eq("fizzbuzz")
+  end
+    it 'returns "fizz" when passed 3' do
     fizz_3 = fizzbuzz(3)
 
     expect(fizz_3).to eq("fizz")
+  end
+  it 'returns "buzz" when passed 5' do 
+    fizz_5 = fizzbuzz(5)
+
+    expect(fizz_5).to eq("buzz")
   end
 end
